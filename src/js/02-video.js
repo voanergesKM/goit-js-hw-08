b07fc9a09d81d55ca49ba94668449a245f6bdd61
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-08-08 17:23:56 +0300

    Task #2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.on('timeupdate', saveLocal);
+
+function saveLocal(data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+}
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+const parsedTime = JSON.parse(savedTime);
+
+player.setCurrentTime(parsedTime.seconds);
