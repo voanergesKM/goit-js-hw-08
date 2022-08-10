e5b59d8c1cd0f90bc7b6bd7b70083f8cf5bf4175
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-08-10 12:31:42 +0300

    Task 2 fixed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,14 +2,16 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const throttle = require('lodash.throttle');
+const LOCAL_KEY = 'videoplayer-current-time';
 
 player.on('timeupdate', throttle(saveLocal, 1000));
 
-function saveLocal(data) {
-  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+function saveLocal({ seconds }) {
+  localStorage.setItem(LOCAL_KEY, seconds);
 }
 
-const savedTime = localStorage.getItem('videoplayer-current-time');
-const parsedTime = JSON.parse(savedTime);
+const savedTime = localStorage.getItem(LOCAL_KEY);
 
-player.setCurrentTime(parsedTime.seconds);
+if (localStorage.getItem(LOCAL_KEY)) {
+  player.setCurrentTime(savedTime);
+}
